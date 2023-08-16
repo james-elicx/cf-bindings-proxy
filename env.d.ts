dbf712635162f4ffe2e0089a1c653d8c9c5fb3c1
James <james@eli.cx>
2023-08-16 01:48:07 +0100

    feat: support for using a custom port

--- env.d.ts
+++ env.d.ts
@@ -4,6 +4,9 @@ declare global {
 			NODE_ENV?: string;
 			DISABLE_BINDINGS_PROXY?: string;
 			ENABLE_BINDINGS_PROXY?: string;
+			BINDINGS_PROXY_PROTOCOL?: string;
+			BINDINGS_PROXY_HOST?: string;
+			BINDINGS_PROXY_PORT?: string;
 		}
 	}
 }
