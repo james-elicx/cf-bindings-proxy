918bb55dfb42d93d359c03e6368dd3e172f37be6
James <james@eli.cx>
2023-08-21 12:08:00 +0100

    feat: option for custom fallback instead of `process.env` (#25)

---
+++ .changeset/chatty-rocks-occur.md
@@ -0,0 +1,5 @@
+---
+'cf-bindings-proxy': minor
+---
+
+Support for providing a custom fallback to use instead of `process.env`.
--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,20 @@ import { createBindingProxy } from './proxy';
  * const value = await binding<KVNamespace>('MY_KV').get('key');
  * ```
  *
+ * @example
+ * By default, `process.env` is used in production, however, a custom fallback can be provided.
+ * ```ts
+ * const MY_KV = binding<KVNamespace>('MY_KV', { fallback: platform.env });
+ * ```
+ *
  * @param id Binding ID.
+ * @param opts Binding options, such as a custom fallback.
  * @returns Binding value.
  */
-export const binding = <T>(id: string): T => {
+export const binding = <T>(id: string, opts?: BindingOpts): T => {
 	if (
-		process.env.ENABLE_BINDINGS_PROXY ||
-		(!process.env.DISABLE_BINDINGS_PROXY && process.env.NODE_ENV === 'development')
+		process?.env?.ENABLE_BINDINGS_PROXY ||
+		(!process?.env?.DISABLE_BINDINGS_PROXY && process?.env?.NODE_ENV === 'development')
 	) {
 		return new Proxy(
 			{},
@@ -24,5 +31,9 @@ export const binding = <T>(id: string): T => {
 		) as T;
 	}
 
-	return process.env[id] as T;
+	return (opts?.fallback ?? process?.env)?.[id] as T;
+};
+
+type BindingOpts = {
+	fallback: Record<string, unknown>;
 };
